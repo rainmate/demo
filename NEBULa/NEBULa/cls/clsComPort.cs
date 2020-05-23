13f62f35b8d46a338e02e98ab8e5eb4a55435a9a
coffee <zzhenzhe@163.com>
2020-05-23 19:33:20 +0800

    0523 add NEBULa

---
+++ NEBULa/NEBULa/cls/clsComPort.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NEBULa.cls
+{
+    class clsComPort
+    {
+        public static bool InitCOM(SerialPort ComPort, int Port)
+        {
+            try
+            {
+                ComPort.PortName = "COM" + Port.ToString();
+                ComPort.BaudRate = 115200;
+                ComPort.Parity = Parity.None;
+                ComPort.DataBits = 8;
+                ComPort.StopBits = StopBits.One;
+                ComPort.ReadBufferSize = 2046;
+                //ComPort.Handshake = Handshake.RequestToSend;
+                //ComPort.ReceivedBytesThreshold = 10;
+
+                ComPort.NewLine = Environment.NewLine;
+                ComPort.DtrEnable = true;
+                ComPort.RtsEnable = true;
+                ComPort.ReadTimeout = 20;
+                ComPort.WriteTimeout = 20;
+
+                ComPort.Open();
+                return true;
+            }
+            catch (Exception
+
+                e)
+            {
+                return false;
+            }
+        }
+
+
+        public static void close(SerialPort ComPort)
+        {
+            try
+            {
+                if (null != ComPort)
+                {
+                    try
+                    {
+                        ComPort.Close();
+                    }
+                    catch
+                    {
+                    }
+                    ComPort = null;
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+}
---
+++ NEBULa/NEBULa/formLogin.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace NEBULa
+{
+    partial class formLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formLogin));
+            this.label1 = new System.Windows.Forms.Label();
+            this.comName = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.butLogin = new System.Windows.Forms.Button();
+            this.butClose = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(225, 199);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "用户名：";
+            // 
+            // comName
+            // 
+            this.comName.FormattingEnabled = true;
+            this.comName.Location = new System.Drawing.Point(325, 193);
+            this.comName.Name = "comName";
+            this.comName.Size = new System.Drawing.Size(224, 26);
+            this.comName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(225, 276);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "密码：";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(329, 268);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(219, 28);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // butLogin
+            // 
+            this.butLogin.Location = new System.Drawing.Point(179, 362);
+            this.butLogin.Name = "butLogin";
+            this.butLogin.Size = new System.Drawing.Size(160, 45);
+            this.butLogin.TabIndex = 4;
+            this.butLogin.Text = "登 录";
+            this.butLogin.UseVisualStyleBackColor = true;
+            this.butLogin.Click += new System.EventHandler(this.butLogin_Click);
+            // 
+            // butClose
+            // 
+            this.butClose.Location = new System.Drawing.Point(410, 362);
+            this.butClose.Name = "butClose";
+            this.butClose.Size = new System.Drawing.Size(160, 45);
+            this.butClose.TabIndex = 5;
+            this.butClose.Text = "退 出";
+            this.butClose.UseVisualStyleBackColor = true;
+            this.butClose.Click += new System.EventHandler(this.butClose_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(750, 151);
+            this.panel1.TabIndex = 6;
+            // 
+            // formLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(750, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.butClose);
+            this.Controls.Add(this.butLogin);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comName);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "formLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "formLogin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button butLogin;
+        private System.Windows.Forms.Button butClose;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
